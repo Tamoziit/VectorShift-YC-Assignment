167cd6276054515cea4affa1ce521243216fe6a2
Tamojit Das <tamojitdas181007@gmail.com>
2026-01-31 17:31:17 +0530

    App setup & modularization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Pipeline from './pages/Pipeline';
+
+function App() {
+  return (
+    <div>
+      <Pipeline />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Pipeline.jsx
@@ -0,0 +1,15 @@
+import { SubmitButton } from "../components/submit";
+import { PipelineToolbar } from "../components/toolbar";
+import { PipelineUI } from "../components/ui";
+
+const Pipeline = () => {
+	return (
+		<div>
+			<PipelineToolbar />
+			<PipelineUI />
+			<SubmitButton />
+		</div>
+	)
+}
+
+export default Pipeline;
