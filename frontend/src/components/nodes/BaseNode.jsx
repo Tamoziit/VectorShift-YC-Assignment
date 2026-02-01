f76bb971c44f44f0cad67b1b7389861c12736534
Tamojit Das <tamojitdas181007@gmail.com>
2026-02-01 18:54:57 +0530

    UI updates: 4

--- frontend/src/components/nodes/BaseNode.jsx
+++ frontend/src/components/nodes/BaseNode.jsx
@@ -3,17 +3,29 @@ import { Handle } from "reactflow";
 const BaseNode = ({
 	id,
 	title,
+	icon,
 	handles = [],
 	children,
 }) => {
 
 	return (
-		<div className={`${title} w-50 border-2 border-gray-300 rounded-lg p-3`}>
-			{title && (
-				<h1 className="dark-header">
-					{title}
-				</h1>
-			)}
+		<div className={`${title} w-50 border-2 border-gray-300 rounded-lg p-3 flex flex-col gap-1.5`}>
+			<div className="flex flex-col">
+				<div className="flex items-center gap-2 w-full">
+					{icon && icon}
+
+					{title && (
+						<h1 className="dark-header text-base">
+							{title}
+						</h1>
+					)}
+				</div>
+
+				<div className="text-sm w-full -mt-1">
+					<span className="text-yellow-100">id: </span>
+					<span className="text-gray-200">{id}</span>
+				</div>
+			</div>
 
 			{handles.map((handle) => (
 				<Handle
@@ -25,7 +37,7 @@ const BaseNode = ({
 				/>
 			))}
 
-			<div>
+			<div className="w-full flex flex-col gap-1">
 				{children}
 			</div>
 		</div>
